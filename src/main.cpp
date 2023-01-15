d5f867abc2bbbafe35cdef9002d03efdb8e5b267
ricobalakit <rico@balak.it>
2023-01-14 22:30:24 -0600

    first commit with stuff

---
+++ src/main.cpp
@@ -0,0 +1,172 @@
+﻿#include <main.h>
+
+ESP32Timer ITimer(1);
+ESP32_PWM ISR_PWM;
+
+bool IRAM_ATTR TimerHandler(void *timerNo)
+{
+    ISR_PWM.run();
+
+    return true;
+}
+
+void DisplayText(float timeSeconds, String line0, String line1, String line2, String line3, String line4, String line5)
+{
+    if (!_displayEnabledAndFound)
+    {
+        return;
+    }
+
+    Display.clearDisplay();
+    Display.setTextSize(1);
+    Display.setTextColor(WHITE);
+
+    Display.setCursor(0, 0);
+    Display.println(line0);
+
+    Display.setCursor(0, 10);
+    Display.println(line1);
+
+    Display.setCursor(0, 20);
+    Display.println(line2);
+
+    Display.setCursor(0, 30);
+    Display.println(line3);
+
+    Display.setCursor(0, 40);
+    Display.println(line4);
+
+    Display.setCursor(0, 50);
+    Display.println(line5);
+
+
+    Display.display();
+
+    delay(1000 * timeSeconds);
+
+}
+
+// format for control:
+// start byte is security. end byte is security. 2nd byte is heartbeat.
+// 6 pairs, frequency (1hz to 256hz) and duration (0ms to 1024ms), in bytes
+//[secbyte][heartbeat4][heartbeat3][heartbeat2][heartbeat1][freq0][dur0][freq1][dur1][freq2][dur2][freq3][dur3][freq4][dur4][freq5][dur5][secbyte]
+
+void InitializeDisplay()
+{
+    if (!Display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    {
+        _displayEnabledAndFound = false;
+
+        Serial.println("no display");
+    }
+    else
+    {
+        DisplayText(1, "45654645");
+        _displayEnabledAndFound = true;
+
+        _line0 = "";
+        _line1 = "";
+        _line2 = "";
+        _line3 = "";
+        _line4 = "";
+        _line5 = "";
+
+        Serial.println("found display");
+    }
+    
+    
+}
+
+void setup()
+{
+// write your initialization code here
+    Serial.begin(115200);
+
+    delay(1000);
+
+    Wire.begin();
+
+    delay(1000);
+    
+    InitializeDisplay();
+
+    // Interval in microsecs
+    if (ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_US, TimerHandler))
+    {
+        startMicros = micros();
+    }
+
+    // not 5, 14, or 15
+
+    // safe: PIN_D25, PIN_D12, PIN_D16, PIN_D17, PIN_D18, PIN_D19
+
+    pinMode(PIN_GREY, OUTPUT);
+    pinMode(PIN_ORANGE, OUTPUT);
+    pinMode(PIN_YELLOW, OUTPUT);
+    pinMode(PIN_GREEN, OUTPUT);
+    pinMode(PIN_BLUE, OUTPUT);
+    pinMode(PIN_PURPLE, OUTPUT);
+
+    ISR_PWM.setPWM(PIN_GREY, 2.083333, 4);
+    ISR_PWM.setPWM(PIN_ORANGE, 4, 8);
+    ISR_PWM.setPWM(PIN_YELLOW, 8, 16);
+    ISR_PWM.setPWM(PIN_GREEN, 16, 32);
+    ISR_PWM.setPWM(PIN_BLUE, 32, 50);
+    ISR_PWM.setPWM(PIN_PURPLE, 64, 50);
+
+    pinMode(PIN_D2, OUTPUT);
+
+
+}
+
+void loop()
+{
+    DisplayText(1, String(micros()));
+    /*
+    if(millis()%2000 > 1000)
+    {
+        ISR_PWM.enableAll();
+    }
+    else
+    {
+        ISR_PWM.disableAll();
+    }
+     */
+// write your code here
+    /*
+    if(millis()%2000 > 1000)
+    {
+        Serial.print("on");
+        
+        digitalWrite(PIN_D2, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+        ISR_PWM.enable(PIN_D15);
+        ISR_PWM.enable(PIN_D4);
+        ISR_PWM.enable(PIN_D5);
+        ISR_PWM.enable(PIN_D12);
+        ISR_PWM.enable(PIN_D13);
+        ISR_PWM.enable(PIN_D14);
+        
+        ISR_PWM.setPWM(PIN_D15, 4, 50);
+        ISR_PWM.setPWM(PIN_D4, 4, 50);
+        ISR_PWM.setPWM(PIN_D5, 4, 50);
+        ISR_PWM.setPWM(PIN_D12, 4, 50);
+        ISR_PWM.setPWM(PIN_D13, 4, 50);
+        ISR_PWM.setPWM(PIN_D14, 4, 50);
+    }
+    else
+    {
+        Serial.print("off");
+
+
+        digitalWrite(PIN_D2, LOW);    // turn the LED off by making the voltage LOW
+
+        ISR_PWM.disable(PIN_D15);
+        ISR_PWM.disable(PIN_D4);
+        ISR_PWM.disable(PIN_D5);
+        ISR_PWM.disable(PIN_D12);
+        ISR_PWM.disable(PIN_D13);
+        ISR_PWM.disable(PIN_D14);
+    }
+     */
+}
---
+++ src/main.h
@@ -0,0 +1,52 @@
+﻿#include <Arduino.h>
+#include <ESP32_PWM.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define NUMBER_ISR_PWMS         16
+
+#define PIN_D0            0         // Pin D0 mapped to pin GPIO0/BOOT/ADC11/TOUCH1 of ESP32
+#define PIN_D1            1         // Pin D1 mapped to pin GPIO1/TX0 of ESP32
+#define PIN_D2            2         // Pin D2 mapped to pin GPIO2/ADC12/TOUCH2 of ESP32
+#define PIN_D3            3         // Pin D3 mapped to pin GPIO3/RX0 of ESP32
+#define PIN_D4            4         // Pin D4 mapped to pin GPIO4/ADC10/TOUCH0 of ESP32
+#define PIN_D5            5         // Pin D5 mapped to pin GPIO5/SPISS/VSPI_SS of ESP32
+#define PIN_D6            6         // Pin D6 mapped to pin GPIO6 of ESP32
+#define PIN_D7            7         // Pin D7 mapped to pin GPIO7 of ESP32
+#define PIN_D8            8         // Pin D8 mapped to pin GPIO8 of ESP32
+#define PIN_D9            9         // Pin D9 mapped to pin GPIO9 of ESP32
+#define PIN_D10           10        // Pin D10 mapped to pin GPIO10 of ESP32
+#define PIN_D11           11        // Pin D11 mapped to pin GPIO11 of ESP32
+#define PIN_D12           12        // Pin D12 mapped to pin GPIO12/HSPI_MISO/ADC15/TOUCH5/TDI of ESP32
+#define PIN_D13           13        // Pin D13 mapped to pin GPIO13/HSPI_MOSI/ADC14/TOUCH4/TCK of ESP32
+#define PIN_D14           14        // Pin D14 mapped to pin GPIO14/HSPI_SCK/ADC16/TOUCH6/TMS of ESP32
+#define PIN_D15           15        // Pin D15 mapped to pin GPIO15/HSPI_SS/ADC13/TOUCH3/TDO of ESP32
+#define PIN_D16           16        // Pin D16 mapped to pin GPIO16/TX2 of ESP32
+#define PIN_D17           17        // Pin D17 mapped to pin GPIO17/RX2 of ESP32     
+#define PIN_D18           18        // Pin D18 mapped to pin GPIO18/VSPI_SCK of ESP32
+#define PIN_D19           19        // Pin D19 mapped to pin GPIO19/VSPI_MISO of ESP32
+
+#define PIN_D21           21        // Pin D21 mapped to pin GPIO21/SDA of ESP32
+#define PIN_D22           22        // Pin D22 mapped to pin GPIO22/SCL of ESP32
+#define PIN_D23           23        // Pin D23 mapped to pin GPIO23/VSPI_MOSI of ESP32
+#define PIN_D25           25        // Pin D25 mapped to pin GPIO25/ADC18/DAC1 of ESP32
+#define PIN_D26           26        // Pin D26 mapped to pin GPIO26/ADC19/DAC2 of ESP32
+#define PIN_D27           27    
+
+#define PIN_GREY            PIN_D16
+#define PIN_ORANGE          PIN_D17
+#define PIN_YELLOW          PIN_D19
+#define PIN_GREEN           PIN_D4
+#define PIN_BLUE            PIN_D13
+#define PIN_PURPLE          PIN_D18
+                                
+#define HW_TIMER_INTERVAL_US      20L// Pin D27 mapped to pin GPIO27/ADC17/TOUCH7 of ESP32   
+uint32_t startMicros = 0;
+
+Adafruit_SSD1306 Display(-1); //-1 arg means no reset pin
+bool _displayEnabledAndFound = false;
+String _line0, _line1, _line2, _line3, _line4, _line5;
+
+void DisplayText(float timeSeconds, String line0 = "", String line1 = "", String line2 = "", String line3 = "",
+                 String line4 = "", String line5 = "");
